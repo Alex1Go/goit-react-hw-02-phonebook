3af6d1a1f083dce2df344cfe3d8ff11c3dc8ad08
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-08-14 16:20:14 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,24 +8,26 @@ import { ContactList } from "./ContactList/ContactList";
 export class App extends Component {
   state = {
     contacts: [
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-  filter: '',
-  name: '',
-  number: ''
+    filter: '',
+    name: '',
+    number: ''
   };
 
   addContact = newContact => {
+    const isExist = this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase());
+    if (isExist) {
+        alert(`${newContact.name} is already in contacts.`);
+        return
+      };
+    
     this.setState(prevState => {
       return {
         contacts: [...prevState.contacts, newContact],
-        // if(isExist) {
-        //   alert(`${name} is already in contacts.`);
-        //   return
-        // };
       };
     });
   };
@@ -47,7 +49,7 @@ export class App extends Component {
 
   render() {
     const { contacts } = this.state;
-    const realItemContacts = contacts.filter(contact => contact.name.toLocaleLowerCase().includes(this.state.filter));
+    const realItemContacts = contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter));
 
     return (
       <div>
